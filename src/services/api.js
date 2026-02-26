d4cf05f155db1c8af7832a57019604e086b745fe
awololaeinola-lang <awololaeinola@gmail.com>
2026-02-26 01:45:28 -1000

    Update API URL to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://skycast-backend-1.onrender.com/api",
 });
 
 export const getWeather = async (city) => {
