13720227e1990577fb404b74df8f7452ccd77f21
awololaeinola-lang <awololaeinola@gmail.com>
2026-02-27 01:12:35 -1000

    Fix production API baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,16 @@ const API = axios.create({
   baseURL: "https://skycast-backend-1.onrender.com/api",
 });
 
-export const getWeather = async (city) => {
-  return API.get(`/weather?city=${city}`);
-}; 
-// Redeploy trigger
+export const getWeather = async (city, lat, lon) => {
+  if (city) {
+    const res = await API.get(`/weather?city=${city}&units=metric`);
+    return res.data;
+  }
+
+  if (lat && lon) {
+    const res = await API.get(
+      `/weather?lat=${lat}&lon=${lon}&units=metric`
+    );
+    return res.data;
+  }
+};
